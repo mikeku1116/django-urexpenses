2e3e904ac188d027c6f356749103f98a4b830b88
mikeku <mikekuforgit@gmail.com>
2020-03-29 17:21:47 +0800

    完成expenses新增資料操作及查詢所有資料

--- expenses/views.py
+++ expenses/views.py
@@ -1,13 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Expense
 from .forms import ExpenseModelForm
 
 
 def index(request):
 
+    expenses = Expense.objects.all()  # 查詢所有資料
+
     form = ExpenseModelForm()
 
+    if request.method == "POST":
+        form = ExpenseModelForm(request.POST)
+        if form.is_valid:
+            form.save()
+        return redirect("/expenses")
+
     context = {
+        'expenses': expenses,
         'form': form
     }
 
-    return render(request, 'index.html', context)
+    return render(request, 'expenses/index.html', context)
