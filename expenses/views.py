81f1c057ab37cb8c6169ca665f4ab94ab019d60a
mikeku <mikekuforgit@gmail.com>
2020-03-29 17:13:57 +0800

    expenses首頁畫面建立「新增花費項目」的表單

--- expenses/views.py
+++ expenses/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .forms import ExpenseModelForm
 
 
 def index(request):
 
-    return HttpResponse("Hello Django")
+    form = ExpenseModelForm()
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'index.html', context)
